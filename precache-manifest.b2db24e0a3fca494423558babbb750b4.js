9913f90e9aee252613d132c2c1e2489bef1f9d7a
Rick <rmansfield@realpeoplesratings.com>
2021-07-15 22:00:30 -0500

    Updates

---
+++ precache-manifest.b2db24e0a3fca494423558babbb750b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be5206db85db83d443e30f440dfcdfe",
+    "url": "/nasa-photo-of-the-day/index.html"
+  },
+  {
+    "revision": "cea737cf4c241db66405",
+    "url": "/nasa-photo-of-the-day/static/css/main.cc6235dc.chunk.css"
+  },
+  {
+    "revision": "f80a42e04a26a80bfcd9",
+    "url": "/nasa-photo-of-the-day/static/js/2.51ca67d2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/nasa-photo-of-the-day/static/js/2.51ca67d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea737cf4c241db66405",
+    "url": "/nasa-photo-of-the-day/static/js/main.5900aa9b.chunk.js"
+  },
+  {
+    "revision": "1029f2261087281c91c9",
+    "url": "/nasa-photo-of-the-day/static/js/runtime-main.82a31dc6.js"
+  }
+]);
